642410e38799d8dc3ece3373b2ae5986137281c2
yuhualee <550807257@qq.com>
2015-04-09 14:32:19 +0800

    swipe2.html

---
+++ h5app/app/scripts/swipe2.js
@@ -0,0 +1,31 @@
+/* jshint node:true */
+'use strict';
+
+/*global $:false,Zepto:false */
+// ;(function($,undefined){
+// })(Zepto);
+
+$(function(){
+	var winH = $(window).height();
+	var winW = $(window).width();
+
+	$(".wrap .topPic").height(winH);
+	
+	$(".wrap .topPic").on("tap",function(){
+		if($(this).height() == winH){
+			$(this).height(winW*0.63);
+		}else{
+			$(this).height(winH);
+		}
+	});
+	$(".wrap .topPic").on("swipeUp",function(event){
+			$(this).height(winW*0.63);
+	});
+	$(".wrap .topPic").on("swipeDown",function(){
+		$(this).height(winH);
+	});
+	document.addEventListener('touchmove', function () {
+				event.preventDefault();
+			}, false);
+	
+});
