8968d6555bf7836c9c16d8c129adc958f7759fac
yuhualee <550807257@qq.com>
2015-04-09 18:48:10 +0800

    3

--- h5app/app/scripts/swipe3.js
+++ h5app/app/scripts/swipe3.js
@@ -7,16 +7,21 @@
 
 $(function(){
 
-	console.log("--- start");
-
 	var startPostion = 0, moveDistanceY;
 	var img = $(".wrap .topPic");
 	var biggerH = $(window).height();
 	var smallerH = $(window).width()*0.63;
+	
+
+	img.height(biggerH);
 	var imgHeight = img.height();
 
+	var text = img.children(".text");
+	var opacity;
 
 	$(img).on('touchstart',function(e){
+
+		imgHeight = img.height();
 
 		// e.preventDefault();
 		var touchs = e.changedTouches[0];
@@ -28,17 +33,42 @@ $(function(){
 		var touchs = e.changedTouches[0];
 		moveDistanceY = touchs.pageY - startPostion;
 
-		// move(-moveDistanceY);
-
-		console.log(img, moveDistanceY, imgHeight);
-
 		imgHeight = imgHeight + moveDistanceY;
 
 		img.height(imgHeight);
+		 
 
+		// opacity
+		if(moveDistanceY > 0){
+			opacity = imgHeight / (biggerH-smallerH);
+			text.css("opacity",opacity);
+		}else{
+			opacity = (biggerH - smallerH )/imgHeight;
+			text.css("opacity",1-opacity);
+		}
+		// img
+
+		
 		startPostion = touchs.pageY;
 
+	}).on('touchend',function(){
+		if(moveDistanceY > 0){
+			img.height(biggerH);
+			text.css("opacity",1);
+		}else{
+			img.height(smallerH);
+			text.css("opacity",0);
+		}
 	});
+
+
+	// $(".wrap .topPic").on("tap",function(){
+	// 	if($(this).height() == biggerH){
+	// 		$(this).height(smallerH);
+	// 	}else{
+	// 		$(this).height(biggerH);
+	// 	}
+	// });
 
 	/*
 	$(document).on("swipeUp",function(){
